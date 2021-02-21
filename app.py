7c8c3c082ab594cee60f0e6a9b78179483f607ac
charusm18 <charusm18@gmail.com>
2021-02-21 11:47:54 -0500

    deploy

--- app.py
+++ app.py
@@ -35,6 +35,6 @@ def analysis():
 purpose: Run server.
 """
 if __name__ == '__main__':
-   app.set( 'port', ( process.env.PORT || 5000 ));
-   app.run(threaded=True, port=5000)
+   port = int(os.environ.get('PORT', 33507))
+   app.run(debug=True, port=33507)
    
